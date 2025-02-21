ce0f7df77d7dcaafe3f6c636be4930d12659dee3
Erin Shawhan <ekshawhan@gmail.com>
2025-02-20 19:54:09 -0600

    set up asp.net app

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace capstone_team11.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
